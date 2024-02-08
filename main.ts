dd85ea61a24df3f95c25ed92c5b2c984379b94e4
Michael Chenevey <mchenevey7@gmail.com>
2024-02-08 15:55:43 -0800

    initial plugin commit

--- main.ts
+++ main.ts
@@ -1,85 +1,76 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { NotionUtil } from 'notion-util';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	notionApiKey: string;
+	notionTaskDb: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	notionApiKey: 'apiKey',
+	notionTaskDb: 'tableId'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SoupsTasksPlugin extends Plugin {
+	settings: PluginSettings;
+	notionUtil: NotionUtil;
 
 	async onload() {
 		await this.loadSettings();
+		this.notionUtil = new NotionUtil(this);
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('arrow-up-from-line', 'Update task', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Uploading task!');
+			// get current file n stuff
+			const currentFile = this.app.workspace.getActiveFile()
+			if (currentFile != null) {
+				this.invokeNotionUtilOnFile(currentFile);
+			} else {
+				new Notice('Sorry, no file to read!');
+			}
+			new Notice('Done!');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText("Soup's Tasks");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.addSettingTab(new SettingTab(this.app, this));
+
+		// Post tasks every minute
+		this.registerInterval(window.setInterval(() => {
+			// ToDo: partial refresh when there are too many tasks
+			statusBarItemEl.setText("Soup's Tasks refreshing...");
+			this.invokeNotionUtil();
+			statusBarItemEl.setText("Soup's Tasks");
+		}, 1 * 60 * 1000));
+
+		// Clean up notion pages when the task is deleted
+		this.app.vault.on("delete", async file => {
+			if (file instanceof TFile) {
+				const metadata = this.app.metadataCache.getFileCache(file)?.frontmatter;
+        		if (metadata != undefined) await this.notionUtil.cleanUpTask(metadata);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		})
 	}
 
 	onunload() {
 
+	}
+
+	async invokeNotionUtilOnFile(file: TFile) {
+		this.notionUtil.createOrUpdateTask(file);
+	}
+
+	async invokeNotionUtil() {
+		const allFiles = this.app.vault.getMarkdownFiles();
+		const currentFile = this.app.workspace.getActiveFile();
+		// Don't mess with the user's active file
+		if (currentFile) allFiles.remove(currentFile);
+		allFiles.forEach(async file =>  {
+			await this.invokeNotionUtilOnFile(file);
+		});
 	}
 
 	async loadSettings() {
@@ -91,26 +82,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: SoupsTasksPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SoupsTasksPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +96,24 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Notion API key')
+			.setDesc('Set your workflow api key here')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your api key')
+				.setValue(this.plugin.settings.notionApiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.notionApiKey = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('Notion Database Id')
+			.setDesc('Set the Id of your tasks table here')
+			.addText(text => text
+				.setPlaceholder('Enter your table id')
+				.setValue(this.plugin.settings.notionTaskDb)
+				.onChange(async (value) => {
+					this.plugin.settings.notionTaskDb = value;
 					await this.plugin.saveSettings();
 				}));
 	}
---
+++ notion-util.ts
@@ -0,0 +1,190 @@
+import { requestUrl, TFile, FrontMatterCache } from "obsidian";
+import { Client } from "@notionhq/client";
+import { markdownToBlocks,  } from "@tryfabric/martian";
+import * as yamlFrontMatter from "yaml-front-matter";
+import SoupsTasksPlugin from "main";
+
+let NOTION_URL = `https://api.notion.com/v1`
+let STATUS_MAPPING: Record<string, string> = {
+    "Not started": "open",
+    "In progress": "open",
+    "Done": "closed"
+}
+let UPDATE_PAGE_CONTENT = false;
+
+export class NotionUtil {
+    plugin: SoupsTasksPlugin;
+    notion: Client;
+    agent: any;
+    constructor(plugin: SoupsTasksPlugin) {
+        this.plugin = plugin;
+        this.notion = new Client({ auth: plugin.settings.notionApiKey })
+    }
+
+    async createTask(file: TFile) {
+        // creates a notion page with the files contents,
+        // adds the notion id to the page's frontmatter yaml
+        const contentBlocks = await this.composePage(file);
+        const metadata = this.plugin.app.metadataCache.getFileCache(file)?.frontmatter;
+        const body = {
+            parent: {database_id: this.plugin.settings.notionTaskDb},
+            properties: {
+                Name: {title: [{text: {content: file.basename,},},],},
+                Tags: {multi_select: [{"name": "michael"}],},
+                Status: {"status": {"name": metadata?.tags.contains("open") ? "Not started" : "Done"}}
+            },
+            children: contentBlocks
+        }
+        const res = await requestUrl({
+            url: `${NOTION_URL}/pages`,
+            method: 'POST',
+            headers: this.getHeaders(),
+            body: JSON.stringify(body)
+        });
+        return res.json['url'];
+    }
+
+    async updateTask(file: TFile, notionId: string) {
+        // get page status and update file if applicable
+        const page = await requestUrl({
+            url: `${NOTION_URL}/pages/${notionId}`,
+            method: 'GET',
+            headers: this.getHeaders()
+        }).catch(async e => {
+            console.log(e);
+            if (e.message.contains('Request failed, status 404')) {
+                // clear link
+                this.writeOrUpdateMetadata(file, "link", "");
+            }
+        });
+        if (!page) {
+            return;
+        }
+        const status = page.json['properties']['Status']['status']['name'];
+        const new_status = this.updateStatus(file, status);
+        if (status != new_status) {
+            const res = await requestUrl({
+                url: `${NOTION_URL}/pages/${notionId}`,
+                method: 'PATCH',
+                headers: this.getHeaders(),
+                body: JSON.stringify({
+                    "properties": {
+                        "Status": {
+                            "status": {
+                                "name": new_status
+                            }
+                        }
+                    }
+                })
+            });
+        }
+
+        if (UPDATE_PAGE_CONTENT) {
+            // Clear out the old page
+            const resOldChildren = await requestUrl({
+                //ToDo: paginate
+                url: `${NOTION_URL}/blocks/${notionId}/children?page_size=100`,
+                method: 'GET',
+                headers: this.getHeaders()
+            })
+            // ToDo: handle notion page deleted/DNE
+            // we have to do this sequentially or risk 409s
+            for (const block of resOldChildren.json['results']) {
+                const resp = await requestUrl({
+                    url: `${NOTION_URL}/blocks/${block['id']}`,
+                    method: 'DELETE',
+                    headers: this.getHeaders()
+                })
+            }
+            // Add new content
+            const new_kids = await this.composePage(file);
+            const body = {
+                "children": new_kids
+            }
+            const response = await requestUrl({
+                url: `${NOTION_URL}/blocks/${notionId}/children`,
+                method: 'PATCH',
+                headers: this.getHeaders(),
+                body: JSON.stringify(body)
+            })
+            return response;
+        }
+    }
+
+    async composePage(file: TFile) {
+        // ToDo: should this use app.metadataCache?
+        const markdown = await this.plugin.app.vault.cachedRead(file);
+        return markdownToBlocks(yamlFrontMatter.loadFront(markdown).__content);
+    }
+
+    async createOrUpdateTask(file: TFile) {
+        const metadata = this.plugin.app.metadataCache.getFileCache(file)?.frontmatter;
+        if (metadata == undefined || !this.isTask(file)) {
+            return;
+        }
+        
+        if (metadata.link != null) {
+            const notionId = this.getIdFromUrl(metadata.link);
+            if (notionId == undefined) {
+                // ToDo: clear the field and recreate instead
+                console.log("Invalid notion link in " + file.path);
+                return;
+            } else {
+                return this.updateTask(file, notionId);
+            }
+        } else {
+            const url = await this.createTask(file);
+            this.writeOrUpdateMetadata(file, 'link', url);
+        }
+    }
+
+    async cleanUpTask(metadata: FrontMatterCache) {
+        if (metadata.link != null) {
+            const notionId = this.getIdFromUrl(metadata.link);
+            if (notionId == undefined) {
+                console.log("Couldn't get id, skipping");
+                return;
+            } else {
+                await requestUrl({
+                    url: `${NOTION_URL}/blocks/${notionId}`,
+                    method: 'DELETE',
+                    headers: this.getHeaders()
+                })
+            }
+        }
+    }
+
+    getIdFromUrl(url: string) {
+        return url.split('/').last()?.split('-').last()
+    }
+
+    getHeaders() {
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + this.plugin.settings.notionApiKey,
+            'Notion-Version': '2022-06-28'
+        }
+    }
+
+    writeOrUpdateMetadata(file: TFile, key: string, value: any) {
+        this.plugin.app.fileManager.processFrontMatter(file, (frontmatter) => {
+            frontmatter[key] = value
+        })
+    }
+
+    isTask(file: TFile) {
+        const metadata = this.plugin.app.metadataCache.getFileCache(file)?.frontmatter;
+        return metadata != undefined && metadata.tags.contains("task")
+    }
+
+    updateStatus(file: TFile, notion_status: string) {
+        const tags = new Set<string>(this.plugin.app.metadataCache.getFileCache(file)?.frontmatter?.tags);
+        const closed = tags.has('closed');
+        tags.delete('open');
+        tags.delete('closed');
+        const new_status = STATUS_MAPPING[notion_status] == "closed" || closed ? "closed" : "open";
+        tags.add(new_status);
+        this.writeOrUpdateMetadata(file, 'tags', Array.from(tags));
+        return new_status == 'closed' ? "Done" : notion_status
+    }
+}
